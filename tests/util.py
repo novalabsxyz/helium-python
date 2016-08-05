d436b698c74922a742f60b9444fb029bf57dbf80
Marc Nijdam <marc.nijdam@gmail.com>
2016-08-05 15:53:12 -0700

    Betamax fixup

--- tests/util.py
+++ tests/util.py
@@ -8,6 +8,20 @@ import helium
 
 Betamax.register_serializer(pretty_json.PrettyJSONSerializer)
 Betamax.register_request_matcher(json_body.JSONBodyMatcher)
+if os.environ.get('TRAVIS'):
+    API_TOKEN = 'X'*10
+else:
+    API_TOKEN = os.environ.get('HELIUM_TEST_API_KEY')
+    assert API_TOKEN, 'Please set HELIUM_TEST_API_KEY to a valid API key'
+
+with Betamax.configure() as config:
+    config.cassette_library_dir = 'tests/cassettes'
+    record_mode = 'none' if os.environ.get('TRAVIS') else 'new_episodes'
+    cassette_options = config.default_cassette_options
+    cassette_options['record_mode'] = record_mode
+    cassette_options['serialize_with'] = 'prettyjson'
+    cassette_options['match_requests_on'].append('json-body')
+    config.define_cassette_placeholder('<AUTH_TOKEN>', API_TOKEN)
 
 
 class HeliumMockTestCase(unittest.BetamaxTestCase):
@@ -17,22 +31,7 @@ class HeliumMockTestCase(unittest.BetamaxTestCase):
     SESSION_CLASS = helium.HeliumClient
 
     def setUp(self):
-        if os.environ.get('TRAVIS'):
-            token = 'X'*10
-        else:
-            token = os.environ.get('HELIUM_TEST_API_KEY')
-            assert token, 'Please set HELIUM_TEST_API_KEY to a valid API key'
-        with Betamax.configure() as config:
-            config.cassette_library_dir = 'tests/cassettes'
-            record_mode = 'never' if os.environ.get('TRAVIS') else 'once'
-            config.define_cassette_placeholder('<AUTH_TOKEN>', token)
-
-            cassette_options = config.default_cassette_options
-            cassette_options['record_mode'] = record_mode
-            cassette_options['serialize_with'] = 'prettyjson'
-            cassette_options['match_requests_on'].append('json-body')
-
         super(HeliumMockTestCase, self).setUp()
 
-        self.recorder.session.token_auth(token)
-        self.client = self.recorder.session
+        self.session.token_auth(API_TOKEN)
+        self.client = self.session
