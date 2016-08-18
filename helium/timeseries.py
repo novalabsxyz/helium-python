32df0a04cf80151fab736d55ee2b7bc62d93bccc
Marc Nijdam <marc.nijdam@gmail.com>
2016-08-18 16:35:36 -0700

    Add timeseries docs (#7)

--- helium/timeseries.py
+++ helium/timeseries.py
@@ -9,6 +9,20 @@ from collections import Iterable
 
 
 class DataPoint(Resource):
+    """Data points for timeseries.
+
+    A datapint represents readings for a given :class:`Timeseries`
+    instance and will always have at least the following attributes:
+
+    :port: The port for the datapoint represents a user or system
+        defined hint around what the value means
+
+    :value: The actual reading value, this can be any json value
+
+    :timestamp: A UTC :class:`datetime` representing the time the
+        reading was taken
+
+    """
 
     def _promote_json_attribute(self, attribute, value):
         if attribute == 'timestamp':
@@ -17,6 +31,60 @@ class DataPoint(Resource):
 
 
 class Timeseries(Iterable):
+    """A timeseries readings container.
+
+    Objects of this class represents a single timeseries query. A
+    Timeseries instance will automatically page forward or backward
+    through the pages returned from the Helium API to return data
+    points that fit within the given arguments.
+
+    The timeseries instance is an :class:`Iterable` which can be used
+    to lazily iterate over very large timeseries data sets. The
+    returned timeseries object will not actually start making any
+    requests to the Helium API until you start iterating over it.
+
+    For example, given:
+
+    .. code-block:: python
+
+        @timeseries()
+        class Sensor(Resource):
+            pass
+
+    You can request a timeseries using:
+
+    .. code-block:: python
+
+        from itertools import islice
+
+        # Fetch a sensor
+        timeseries = sensor.timeseries()
+
+        # Get the first 10 readings
+        first10 = list(islice(timeseries, 10))
+
+    Note that each call to ``sensor.timeseries()`` will return a new
+    timeseries object which you can iterate over.
+
+
+    Args:
+
+        session(Session): The session to use for timeseries requests
+        resource_type(string): The type of the resource to fetch timeseries for
+        resource_id(uuid): Id of the resource (if applicable) to fetch
+            timeseries for
+
+    Keyword Args:
+
+        datapoint_class(Resource): The class to use to construct datapoints
+        datapoint_id(uuid): The datapoint id to start the timeseries
+        page_size(int): The size of pages to fetch (defaults to server
+            preference)
+        direction("prev" or "next"): Whether to go backward ("prev") or
+            forward ("next") in time
+
+    """
+
     def __init__(self, session, resource_type, resource_id,
                  datapoint_class=DataPoint,
                  datapoint_id=None,
@@ -39,6 +107,8 @@ class Timeseries(Iterable):
         self._params = params
 
     def __iter__(self):
+        """Construct an iterator for this timeseries"""
+
         session = self._session
         datapoint_class = self._datapoint_class
         direction = self._direction
@@ -66,6 +136,31 @@ class Timeseries(Iterable):
                 json, data, url = _get_json(url)
 
     def post(self, port, value, timestamp=None):
+        """Post a new reading to a timeseries.
+
+        A reading is comprised of a `port`, a `value` and a timestamp.
+
+        A port is like a tag for the given reading and gives an
+        indication of the meaning of the value.
+
+        The value of the reading can be any valid json value.
+
+        The timestamp is considered the time the reading was taken, as
+        opposed to the `created` time of the data-point which
+        represents when the data-point was stored in the Helium
+        API. If the timestamp is not given the server will construct a
+        timestemp upon receiving the new reading.
+
+        Args:
+
+            port(string): The port to use for the new data-point
+            value: The value for the new data-point
+
+        Keyword Args:
+
+            timestamp(:class:`datetime`): An optional :class:`datetime` object
+
+        """
         session = self._session
         datapoint_class = self._datapoint_class
         attributes = {
@@ -80,11 +175,32 @@ class Timeseries(Iterable):
 
 
 def timeseries():
+    """Create a timeseries builder.
+
+    Returns:
+
+        A builder function which, given a class creates a timeseries
+        relationship for that class.
+
+    """
     def method_builder(cls):
+        method_doc = """Fetch the timeseries for this :class:`{0}`.
+
+        Returns:
+
+            The :class:`Timeseries` for this :class:`{0}`
+
+        Keyword Args:
+
+            **kwargs: The :class:`Timeseries` object constructor arguments.
+        """.format(cls.__name__)
+
         def method(self, **kwargs):
             resource_id = None if hasattr(self, '_singleton') else self.id
             return Timeseries(self._session, cls._resource_type(), resource_id,
                               **kwargs)
+
+        method.__doc__ = method_doc
         setattr(cls, 'timeseries', method)
         return cls
     return method_builder
